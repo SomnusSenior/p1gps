2c092de249a598e308146f455024a7dd2c8e3d2a
Leonardo Cardozo Gómez <Leonardo.Cardozo@alu.uclm.es>
2018-10-10 13:32:03 +0200

    Preparando build del order con mongo

--- deploy/docker/Order/drop/run.sh
+++ deploy/docker/Order/drop/run.sh
@@ -5,6 +5,7 @@ do
   echo "$(date) - still trying"
   sleep 1
 done
-echo "$(date) - connected successfully"
+echo "$(date) - ot seems mongo server is connected successfully"
 
-java -jar ordering-*.jar
+java -jar usr/local/app/ordering-service-0.1.0.jar \\
+-spring.config.location=application.properties # o *.jar
